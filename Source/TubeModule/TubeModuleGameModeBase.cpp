dcbcb5efd4ea6297d28b08a46414a8ea60581913
hanzochang <hanzo.chan@gmail.com>
2017-03-16 08:46:54 +0900

    Initialize

---
+++ Source/TubeModule/TubeModuleGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TubeModule.h"
+#include "TubeModuleGameModeBase.h"
+
+
+
+
---
+++ Source/TubeModule/TubeModuleGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TubeModuleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUBEMODULE_API ATubeModuleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
